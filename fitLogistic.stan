8dcb98840af109e53b7ad6a6a63bac919c387981
Diogo Melo <diogro@gmail.com>
2017-04-02 00:47:09 -0300

    logistic fit, extra weight measurement

---
+++ fitLogistic.stan
@@ -0,0 +1,85 @@
+functions {
+    // logistic function
+    real logistic(real time, real A, real mu, real lambda) {
+        real x;
+        x = A/(1 + exp(4 * mu * (lambda - time)/A + 2));
+        return x;
+    }
+}
+
+data {
+    int<lower=0> N; // individuals
+    int<lower=1> K; // traits
+    int<lower = 0, upper = 1> sex[N];
+    int<lower = 1, upper = N> ind[K*N];
+    vector[K*N] time; // times
+    vector[K*N] y; // weights
+}
+
+parameters {
+  real<lower=0> sigma;
+
+  real<lower = 0> A_0;
+  real A_sex;
+  real A_tilde[N];
+  real<lower=0> sigma_A;
+    
+  real<lower = 0> mu_0;
+  real mu_sex;
+  real mu_tilde[N];
+  real<lower=0> sigma_mu;
+  
+  real lambda_0;
+  real lambda_sex;
+  real lambda_tilde[N];
+  real<lower=0> sigma_lambda;
+}
+
+transformed parameters{
+  vector<lower = 0>[N] A;
+  vector<lower = 0>[N] mu;
+  vector[N] lambda;
+  
+  real A_i[N];
+  real mu_i[N];
+  real lambda_i[N];
+  
+  for(n in 1:N){
+    
+    A_i[n] = A_tilde[n] * sigma_A;
+    mu_i[n] = mu_tilde[n] * sigma_mu;
+    lambda_i[n] = lambda_tilde[n] * sigma_lambda;
+    
+    A[n]      = 10 * (A_0 + sex[n] *      A_sex +      A_i[n]);
+    mu[n]     =      mu_0 + sex[n] *     mu_sex +     mu_i[n];
+    lambda[n] =  lambda_0 + sex[n] * lambda_sex + lambda_i[n];
+  }
+  
+}
+
+model {
+  vector[K*N] x;
+  
+  for (i in 1:(N*K)){
+      x[i] = logistic(time[i], A[ind[i]], mu[ind[i]], lambda[ind[i]]);
+  }
+  y ~ normal(x, sigma);
+  
+  A_0 ~ normal(0.0, 5.0);
+  mu_0 ~ normal(0.0, 1.0);
+  lambda_0 ~ normal(0.0, 5.0);
+  
+  A_sex ~ normal(0.0, 1.0);
+  mu_sex ~ normal(0.0, 1.0);
+  lambda_sex ~ normal(0.0, 1.0);
+  
+  sigma ~ cauchy(0, 2.5);
+  
+  sigma_A ~ cauchy(0, 1);
+  sigma_mu ~ cauchy(0, 1);
+  sigma_lambda ~ cauchy(0, 1);
+
+  A_tilde ~ normal(0, 1);
+  mu_tilde ~ normal(0, 1);
+  lambda_tilde ~ normal(0, 1);
+}
--- growthCurveFit.R
+++ growthCurveFit.R
@@ -7,12 +7,12 @@ options(mc.cores = parallel::detectCores())
 
 source("./read_F6_phenotypes.R")
 
-times = c(0, 3, seq(7, 49, 7))
+times = c(0, 3, seq(7, 56, 7))
 
 mus = sapply(1:nrow(weightF6), function(i) coef(gcFitModel(times, weightF6[i,weight_traits], control = grofit.control(model.type = "gompertz"))$nls)["mu"])
 hist(unlist(mus))
 
-TestRun = gcFitModel(times, weightF6[1,weight_traits], control = grofit.control(model.type = "gompertz"))
+TestRun = gcFitModel(times, weightF6[1,weight_traits])
 summary(TestRun$nls)
 plot(TestRun)
 
@@ -26,26 +26,17 @@ stan_data = list(N = N,
                  time = rep(times, each = N),
                  y = as.numeric(as.matrix((weightF6[1:N,weight_traits]))))
 
-pooledFit = stan(file = "fitGrompertzPooled.stan", model_name = "pooled_gompertz", data = stan_data, 
-                 iter = 2000, control = list(adapt_delta = 0.9))
-
-summary(pooledFit)
-
 partialPooledFit = stan(file = "fitGrompertz.stan", model_name = "partial_pooled_gompertz", data = stan_data, 
-                 iter = 2000, control = list(adapt_delta = 0.9))
-
-summary(partialPooledFit, pars = "sigma_mu")
-summary(partialPooledFit, pars = c("A_0"))
-summary(partialPooledFit, pars = c("A", "mu", "lambda"))
+                 iter = 1000, control = list(adapt_delta = 0.8))
 
 plot(partialPooledFit, pars = c("mu_i"))
 plot(partialPooledFit, pars = c("A"))
-plot(partialPooledFit, pars = c("mu_0", "mu_sex"))
+plot(partialPooledFit, pars = c("A_0", "A_sex", "mu_0", "mu_sex"))
 
 coefs = summary(partialPooledFit, pars = c("A_0", "A_sex", "mu_0", "mu_sex", "lambda_0", "lambda_sex"))$summary[,"mean"]
 all_coefs = summary(partialPooledFit, pars = c("A", "mu", "lambda"))$summary[,"mean"]
 
-wide_weight = weightF6[1:N,] %>% gather(trait, value, Weight_D0:Weight_D49) %>% mutate(times = rep(times, each = N))
+wide_weight = weightF6[1:N,] %>% gather(trait, value, Weight_D0:Weight_D56) %>% mutate(times = rep(times, each = N))
 grid <- with(wide_weight, seq(min(times), max(times), length = 100))
 gompertz_mean_curve <- ddply(wide_weight, "Sex", function(df) {
   data.frame( 
@@ -67,7 +58,45 @@ gompertz_ID_curve <- ddply(wide_weight, .(Sex, ID), function(df) {
 }
 )
 
-ggplot(wide_weight, aes(times, value, group = ID)) + geom_jitter(alpha = 0.1) + facet_wrap(~Sex) +
+ggplot(wide_weight, aes(times, value, group = ID)) + geom_text(aes(label = ID), alpha = 1) + facet_wrap(~Sex) +
   geom_line(aes(y = curve), data = gompertz_ID_curve, colour = "gray", alpha = 0.1) + 
   geom_line(aes(y = curve, group = 1), data = gompertz_mean_curve, colour = "red")
   
+
+#### Logistic
+
+partialPooledLogistic = stan(file = "fitLogistic.stan", model_name = "partial_pooled_logistic", data = stan_data, 
+                        iter = 1000, control = list(adapt_delta = 0.9))
+
+plot(partialPooledLogistic, pars = c("mu_i"))
+plot(partialPooledLogistic, pars = c("A"))
+plot(partialPooledLogistic, pars = c("A_0", "A_sex", "mu_0", "mu_sex"))
+
+coefsLogistic = summary(partialPooledLogistic, pars = c("A_0", "A_sex", "mu_0", "mu_sex", "lambda_0", "lambda_sex"))$summary[,"mean"]
+all_coefsLogistic = summary(partialPooledLogistic, pars = c("A", "mu", "lambda"))$summary[,"mean"]
+
+wide_weight = weightF6[1:N,] %>% gather(trait, value, Weight_D0:Weight_D56) %>% mutate(times = rep(times, each = N))
+grid <- with(wide_weight, seq(min(times), max(times), length = 100))
+logistic_mean_curve <- ddply(wide_weight, "Sex", function(df) {
+  data.frame( 
+    times = grid,
+    curve = logistic(grid, A = 10 * (coefsLogistic["A_0"] + ifelse(df$Sex[1] == "M", coefsLogistic["A_sex"], 0)), 
+                     mu = coefsLogistic["mu_0"] + ifelse(df$Sex[1] == "M", coefsLogistic["mu_sex"], 0), 
+                     lambda = coefsLogistic["lambda_0"] + ifelse(df$Sex[1] == "M", coefsLogistic["lambda_sex"], 0))
+  )
+}
+)
+logistic_ID_curve <- ddply(wide_weight, .(Sex, ID), function(df) {
+  id = which(weightF6$ID == df$ID[1])
+  data.frame( 
+    times = grid,
+    curve = logistic(grid, A = all_coefsLogistic[paste0("A[", id, "]")], 
+                     mu = all_coefsLogistic[paste0("mu[", id, "]")], 
+                     lambda = all_coefsLogistic[paste0("lambda[", id, "]")])
+  )
+}
+)
+
+ggplot(wide_weight, aes(times, value, group = ID)) + geom_jitter(alpha = 0.1) + facet_wrap(~Sex) +
+  geom_line(aes(y = curve), data = logistic_ID_curve, colour = "gray", alpha = 0.1) + 
+  geom_line(aes(y = curve, group = 1), data = logistic_mean_curve, colour = "red")
