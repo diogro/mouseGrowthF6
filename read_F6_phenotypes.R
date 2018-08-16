e82afd26072321aec26f172573f9ff414bd767e5
diogro <diogro@gmail.com>
2018-08-16 17:16:56 -0300

    don't know

--- read_F6_phenotypes.R
+++ read_F6_phenotypes.R
@@ -113,8 +113,11 @@ necropsyF6 = full_data_F6 %>% dplyr::select(Litter_ID_new:Sex,
 # dev.off()
 
 mean_growth_strain = ddply(growthStrain, .(Strain), numcolwise(mean))
+mean_growth_F6 = ddply(growthF6, .(Litter_ID_new), numcolwise(mean))
+
 g_means = colMeans(mean_growth_strain[,growth_traits])
 m_growth_strain = gather(mean_growth_strain, period, growth, growth_D0D14:growth_D42D56)
+m_growth_f6 = gather(growthF6, period, growth, growth_D0D14:growth_D42D56)
 m_growth_strain$growth_std = m_growth_strain$growth/g_means[m_growth_strain$period]
 
 m_growth_strain$period = factor(m_growth_strain$period, levels = growth_traits)
@@ -134,12 +137,17 @@ library(kinship2)
 dev.off()
 save_plot("~/Desktop/pedigree.png", drawPedigree(pedigree), base_height = 8)
 
-(stain_growth = ggplot(m_growth_strain, aes(period, growth, group = Strain, color = Strain)) + geom_line(size = 1) + 
-  scale_x_discrete(labels = gsub("growth_", "", growth_traits)) + labs(y = "Growth per interval (g)", x = "") + 
-  geom_abline(slope = 0) + 
+(f6_growth = ggplot() + geom_line(data = m_growth_f6, 
+                                  aes(period, growth, group = ID), 
+                                  size = 0.2, alpha = 0.3, color = 'grey'))
+
+(stain_growth = f6_growth + geom_line(data = m_growth_strain, aes(period, growth, group = Strain, color = Strain), size = 1.5) + 
+    labs(y = "Growth per interval (g)", x = "") + 
     scale_color_discrete(labels = c("A13 - E+", "A22 - E-", "A23 - E-", "A31 - L+", "A41 - L-", "A42 - L-")) + 
     scale_x_discrete(labels = paste0(c("0 to 14", "14 to 28", "28 to 42", "42 to 56"), "\ndays")) + 
   theme_cowplot() + theme(legend.position=c(0.75, 0.80), text = element_text(size=20), axis.text = element_text(size=20)))
-  
+
+
 save_plot("~/Dropbox/labbio/posters/2018 - 07 - 19 - Evolution2018/strain_growth-curves_2w.png", stain_growth,
           base_height = 10, base_aspect_ratio = 1)
+
