965fd9ba3058af776d1891439afc9a6632fe3af3
Diogo Melo <diogro@gmail.com>
2019-03-13 11:57:06 -0300

    paths

--- growthTraitsGMatrix.R
+++ growthTraitsGMatrix.R
@@ -30,10 +30,10 @@ model_growth <- MCMCglmm(as.formula(g_formula),
 summary(model_growth)
 save(model_growth, growthF5F6_sd, file = "./data/growthGfit_2w.Rdata")
 load("./data/growthGfit_2w.Rdata")
-Gs = array(model_growth$VCV[,grep("animal", colnames(model_growth$VCV))], 
+Gs = array(model_growth$VCV[,grep("animal", colnames(model_growth$VCV))],
            c(nrow(model_growth$VCV), n_traits, n_traits))
 Gs = aaply(Gs, 1, `*`, outer(growthF5F6_sd, growthF5F6_sd))
-Rs = array(model_growth$VCV[,grep("units", colnames(model_growth$VCV))], 
+Rs = array(model_growth$VCV[,grep("units", colnames(model_growth$VCV))],
            c(nrow(model_growth$VCV), n_traits, n_traits))
 Rs = aaply(Rs, 1, `*`, outer(growthF5F6_sd, growthF5F6_sd))
 G = apply(Gs, 2:3, mean)
@@ -45,7 +45,7 @@ corrP = cor(residuals(lm(as.matrix(growthF5F6[,growth_traits])~growthF5F6$Sex)))
 
 colnames(corrG) = c("0 to 14\ndays", "14 to 28\ndays", "28 to 42\ndays", "42 to 56\ndays")
 diag(corrG) = 0
-png("/home/diogro/Dropbox/labbio/posters/2018 - 07 - 19 - Evolution2018/growth_Gmatrix_2w_4t.png", width = 2200, height = 2200)
+png("/home/diogro/Dropbox/labbio/articles/TeseDoutorado/chapter_atchley/media/growth_Gmatrix_2w_4t.png", width = 2200, height = 2200)
 par(mfrow=c(1, 1), cex = 4)
 corrplot.mixed(corrG, upper = "ellipse", mar = c(0, 0, 0, 0), cl.lim = c(-0.8, 0.8), addgrid.col = "black", is.corr = FALSE)
 dev.off()
